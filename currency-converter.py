bbc3de5bb9440514cd366185b1af6234d55e1c2a
LEC <leopoldo.espinoza2022@gmail.com>
2025-03-16 14:54:35 +0100

    Update currency-converter.py

--- currency-converter.py
+++ currency-converter.py
@@ -12,8 +12,7 @@ def obtener_tasa_cambio(base, destino):
     response = requests.get(url)    
     datos = response.json()         
     return datos["rates"].get(destino, None)
-
-
+    
 def convertir_moneda(cantidad, base, destino):
     tasa = obtener_tasa_cambio(base, destino)
     if tasa:
@@ -22,17 +21,17 @@ def convertir_moneda(cantidad, base, destino):
         return None
 
 def main():
-    print("Conversor de Monedas")
-    cantidad = float(input("Ingrese la cantidad de dinero: "))
-    base = input("Ingrese la moneda de origen (ej: USD, EUR, JPY, CAD, CNY, AUD, NZD, ZAR, ILS, RUB, INR, ARS, BRL, UYU, MXN, VES): ").upper()
-    destino = input("Ingrese la moneda de destino (ej: USD, EUR, JPY, CAD, CNY, AUD, NZD, ZAR, ILS, RUB, INR, ARS, BRL, UYU, MXN, VES): ").upper()
+    print("Currency Converter")
+    cantidad = float(input("Enter the amount of money: "))
+    base = input("Enter the current currency (ej: USD, EUR, JPY, CAD, CNY, AUD, NZD, ZAR, ILS, RUB, INR, ARS, BRL, UYU, MXN, VES): ").upper()
+    destino = input("Enter the desired currency (ej: USD, EUR, JPY, CAD, CNY, AUD, NZD, ZAR, ILS, RUB, INR, ARS, BRL, UYU, MXN, VES): ").upper()
     
     resultado = convertir_moneda(cantidad, base, destino)
     
     if resultado is not None:
-        print(f"{cantidad} {base} equivale a {resultado:.2f} {destino}")
+        print(f"{cantidad} {base} is equivalent to {resultado:.2f} {destino}")
     else:
-        print("No se pudo obtener la tasa de cambio. Verifique las monedas ingresadas.")
+        print("The exchange rate could not be obtained. Please check the currencies entered.")
 
 if __name__ == "__main__":
     main()
